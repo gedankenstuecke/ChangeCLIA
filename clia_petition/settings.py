7eba3ed22ce5c0e3c5ed7fc5afd649d19b683786
Madeleine Ball <mpball@gmail.com>
2017-05-04 13:34:43 -0700

    Add templates and bootstrap

--- clia_petition/settings.py
+++ clia_petition/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clia_petition',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
